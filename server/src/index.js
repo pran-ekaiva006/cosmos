3c6561d5ad82a69b0e825f66d83342e75a97c351
Pranjal Kumar Verma <pranjalverma975@gmail.com>
2026-04-06 11:44:09 +0530

    added socket.io with modular handler setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./app');
+const socketHandler = require('./socket/socketHandler');
 
 const PORT = 5000;
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+socketHandler(io);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
